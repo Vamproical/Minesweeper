c2f4ac3b2894c57fc33436d9325dde407f9d39d0
Vamproical <mimoun136@yandex.ru>
2020-06-28 18:11:13 +0900

    Complete stage 1.

--- Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/task/src/minesweeper/Main.java
@@ -2,6 +2,14 @@ package minesweeper;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println(".X.......\n" +
+                ".....X..X\n" +
+                "....X....\n" +
+                "......X..\n" +
+                "..X......\n" +
+                "....X....\n" +
+                "..X......\n" +
+                "..X......\n" +
+                "......X..");
     }
 }
